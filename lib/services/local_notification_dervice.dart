dc6d86bfed8317804b04d82476b3ece2f6a62b9a
milad <msherwani92@gmail.com>
2022-02-15 14:55:33 +0300

    initialize the chanel for notification

---
+++ lib/services/local_notification_dervice.dart
@@ -0,0 +1,41 @@
+import 'dart:ffi';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotification{
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin=FlutterLocalNotificationsPlugin();
+
+  static void initialize(){
+
+    final InitializationSettings initializationSettings=
+    InitializationSettings(
+      android:AndroidInitializationSettings("@mipmap/ic_launcher"));
+    _notificationsPlugin.initialize(initializationSettings);
+  }
+
+ static void display(RemoteMessage message)async{
+
+   try {
+     final id=DateTime.now().millisecondsSinceEpoch ~/1000;
+     
+     final NotificationDetails notificationDetails=NotificationDetails(
+       android:AndroidNotificationDetails(
+         "easyapproach",
+         "easyapproach channel"
+         "this is our channel",
+         importance: Importance.max,
+        priority: Priority.high,
+       )
+     );
+     
+       await _notificationsPlugin.show(
+      id,
+      message.notification!.title,
+      message.notification!.body,
+      notificationDetails);
+   } on Exception catch (e) {
+    print(e);
+   }
+ }
+}
